cf2abb6feb3acc8f5d142593ce177eee831bb93e
poolexdee <davidle90@hotmail.com>
2024-04-15 03:01:29 +0200

    add tailwind

---
+++ resources/views/layouts/public/header.blade.php
@@ -0,0 +1,39 @@
+<header class="bg-white">
+    <nav class="flex justify-between items-center w-[92%] mx-auto">
+        <div>
+            <img class="w-16 curoser-pointer" src="https://placehold.co/160x120?text=Hello+World" alt="">
+        </div>
+        <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[30vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-6">
+            <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+                <li>
+                    <a class="hover:text-gray-500" href="#">Hem</a>
+                </li>
+                <li>
+                    <a class="hover:text-gray-500" href="#">CV</a>
+                </li>
+                <li>
+                    <a class="hover:text-gray-500" href="#">Portfolio</a>
+                </li>
+                <li>
+                    <a class="hover:text-gray-500" href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+        <div class="flex items-center gap-6">
+            <button class="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">Logga in</button>
+            <ion-icon name="menu" class="text-3xl cursor-pointer md:hidden onToggleMenu"></ion-icon>
+        </div>
+    </nav>
+</header>
+
+@push('scripts')
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.onToggleMenu').on('click', function () {
+                var newName = ($(this).attr('name') === 'menu') ? 'close' : 'menu';
+                $(this).attr('name', newName);
+                $('.nav-links').toggleClass('top-[5%]');
+            });
+        });
+    </script>
+@endpush
--- resources/views/pages/public/index.blade.php
+++ resources/views/pages/public/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.public')
+@extends('layouts.public.main')
 
 @section('styles')
 @endsection
