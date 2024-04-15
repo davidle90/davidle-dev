2f8eb051ebcfd66187de2c3f9b38acb7769bdaae
poolexdee <davidle90@hotmail.com>
2024-04-16 00:02:06 +0200

    initial commit

--- resources/views/pages/public/index.blade.php
+++ resources/views/pages/public/index.blade.php
@@ -10,14 +10,14 @@
     <div class="container mx-auto">
         <div class="w-4/5 mx-auto">
             <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
-                Template for future projects
+                David Le
             </h1>
             <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-                This is a template for future projects - using Tailwind CSS, Fontawesome / Bootstrap Icons and DropzoneJS. Includes public and admin pages.
+                En engagerad junior utvecklare som strävar efter att bygga smarta och användarvänliga hemsidor av hög kvalitet.
             </p>
             <div class="text-center">
                 <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                    Learn more
+                    Läs mer
                     <i class="fa-solid fa-arrow-right ml-2"></i>
                 </a>
             </div>
