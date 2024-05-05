dc024513ae5a7f64966ea17aa348fc45e7115278
davidle90 <89838376+davidle90@users.noreply.github.com>
2024-05-05 13:45:43 +0200

    add sidebar to admin

--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- <link rel="icon" href="#"/> -->
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -44,15 +45,20 @@
 @yield('modals')
 
 <!-- Main -->
-<main class="flex-grow">
+<main class="grow">
     <!-- Sidebar -->
-    @include('layouts.admin.sidebar')
+    @include('layouts.admin.navbar')
     <div class="sm:ml-64">
         <div class="p-5 bg-gray-100">
             @include('layouts.admin.header')
         </div>
-        <div class="p-5">
-            @yield('content')
+        <div class="flex">
+            <div class="w-52 p-5">
+                @yield('sidebar')
+            </div>
+            <div class="grow p-5">
+                @yield('content')
+            </div>
         </div>
     </div>
 </main>
--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -1,6 +1,5 @@
-
 <aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="{{ route('admin.index')}}" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
